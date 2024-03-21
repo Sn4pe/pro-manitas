d7e282d352b7a9b09fa4196dabdc1f06878cb495
Sn4pe <hi@sn4pe.com>
2024-03-21 19:17:58 +0100

    feat: archivos basicos

---
+++ src/test/java/com/promanitas/promanitas/ProManitasApplicationTests.java
@@ -0,0 +1,13 @@
+package com.promanitas.promanitas;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProManitasApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
