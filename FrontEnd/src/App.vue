f8f3cdbba43cf953d2b6b2d53ef193fb293e14d1
Chuchi Gajate <xechu_68@hotmail.com>
2024-03-30 13:02:51 +0100

    [ADD] estructura frontend y formulario de login

---
+++ FrontEnd/src/App.vue
@@ -0,0 +1,16 @@
+<template>
+
+  <h1>ProManitas</h1>
+  <router-view class="router-vista"/>
+</template>
+
+<script>
+export default {
+  name: "app",
+  components: {},
+};
+</script>
+
+<style>
+
+</style>
