b897b8175bfab8982fc9a70d4064c451c55986d9
Sn4pe <hi@sn4pe.com>
2024-05-02 17:40:26 +0200

    feat: add register design

--- FrontEnd/src/router/index.js
+++ FrontEnd/src/router/index.js
@@ -2,6 +2,7 @@ import { createRouter, createWebHistory } from 'vue-router'
 import HomeView from '@/views/HomeView.vue'
 import User from "@/views/User/User.vue";
 import Login from "@/views/Login.vue";
+import Register from "@/views/Register.vue";
 
 
 const routes = [
@@ -14,6 +15,11 @@ const routes = [
     path: '/login',
     name: 'login',
     component: Login,
+  },
+  {
+    path: '/register',
+    name: 'register',
+    component: Register,
   },
   {
     path: "/admin/usuario",
---
+++ FrontEnd/src/views/Register.vue
@@ -0,0 +1,28 @@
+<script setup>
+
+</script>
+
+<template>
+  <div class="flex justify-center my-20">
+    <form action="" method="post" class="flex flex-col w-1/2 lg:w-1/4">
+      <label for="user" class="py-2">Usuario</label>
+      <input type="text" name="user" id="user" class="inputCustom">
+
+      <label for="email" class="py-2">Email</label>
+      <input type="email" name="email" id="email" class="inputCustom">
+
+      <label for="pass" class="py-2 mt-4">Contraseña</label>
+      <input type="password" name="pass" id="pass" class="inputCustom">
+
+      <button type="button" class="mt-6 py-2 px-4 bg-ligth_orange hover:bg-primary focus:ring-offset-accent text-white w-full transition ease-in duration-200 text-center text-base font-semibold shadow-md focus:outline-none focus:ring-2 focus:ring-offset-2 rounded-lg">
+        Register
+      </button>
+
+    </form>
+  </div>
+
+</template>
+
+<style scoped>
+
+</style>
